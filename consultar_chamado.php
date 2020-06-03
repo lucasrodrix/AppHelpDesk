504c4695f3afcb55992365df52111da70cc17761
Lucas Rodrix <lucasrodrigues1985.lfr@gmail.com>
2020-06-03 13:48:29 -0300

    Recuperando somente os chamados do usuario logado

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -6,7 +6,16 @@
   
   while (!feof($arquivo)) {
     $registro = fgets($arquivo);
-    $chamados[] = $registro;
+    $filter = explode('#',$registro);
+    if (count($filter) > 3) {
+      if($_SESSION['perfil_id'] == 2){
+        if($_SESSION['id'] == $filter[0]){
+          $chamados[] = $registro;
+        }
+      }else{
+        $chamados[] = $registro;
+      }
+    }
   }
   
   fclose($arquivo);
@@ -56,21 +65,12 @@
               <? foreach ($chamados as $chamado){?>
                 <?php
                     $chamado_dados = explode('#',$chamado);
-                    if($_SESSION['perfil_id'] == 2){
-                      if($_SESSION['id'] != $chamado_dados[0]){
-                        continue;
-                      }
-                    }
-                    if(count($chamado_dados) < 3){
-                      continue;
-                    }
                 ?>
                 <div class="card mb-3 bg-light">
                   <div class="card-body">
                     <h5 class="card-title"><?= $chamado_dados[1];?></h5>
                     <h6 class="card-subtitle mb-2 text-muted"><?= $chamado_dados[2];?></h6>
                     <p class="card-text"><?= $chamado_dados[3];?></p>
-                    <h6 class="card-subtitle mb-2 text-muted">Criado por: <?= $chamado_dados[0];?></h6>
 
                   </div>
                 </div>
